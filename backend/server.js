23996d4569f8087c470adb40ec643afccc0a3ed1
Kassy <bongato.kc@gmail.com>
2025-07-02 21:43:33 +0800

    02/07/2025

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+//app configuration
+const app = express();
+const port = 3000;
+
+//middleware configuration
+app.use(express.json());
+
+//API routes
+
+
+//listners
