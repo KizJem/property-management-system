0641838f7ffadfe98f3713ed146ab3a0b6a2b7cf
Kassy <bongato.kc@gmail.com>
2025-07-03 13:42:13 +0800

    07/03/2025

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ const port = 3000;
 app.use(express.json());
 
 //define item list
-
 let itemList = [
     {id:1, name: "name here"},
 
@@ -18,9 +17,37 @@ let itemList = [
 app.get('/api/v1/items', (req, res) =>{
     return res.json(itemList);
 });
-app.post('/api/v1/items', (req, res) =>{});
-app.put('/api/v1/items/:id', (req, res) =>{});
-app.delete('/api/v1/items/:id', (req, res) =>{});
+app.post('/api/v1/items', (req, res) =>{
+    let newItem = {
+        id: itemList.length + 1,
+        name: req.body.name,
+    }
+    itemList.push(newItem);
+    res.status(201).json(newItem)
+});
+app.put('/api/v1/items/:id', (req, res) =>{
+    let itemID = req.params.id;
+    let updatedItem = req.body;
+    let index = itemList.findIndex(item => item.id === itemId);
+
+    if(index !==-1){
+        itemList[index]  updatedItem;
+        res.json(updatedItem);
+    }else{
+        res.status(404).json({message:"Item not found"});
+    }
+});
+app.delete('/api/v1/items/:id', (req, res) =>{
+    let itemId = req.params.id;
+    let index = itemList.findIndex(item => item.id === itemId);
+
+    if(index != -1){
+        let deletedItem = itemList.splice(index,1);
+        res.json(deletedItem[0]);
+    }else{
+        res.status(404).json({message:"Item not found"});
+    }
+});
 
 //listners
 app.listen(port , ()=>{
